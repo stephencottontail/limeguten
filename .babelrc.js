15c57054d57de4a43e9164a276809023435db69c
Stephen Dickinson <stephencottontail@me.com>
2019-01-20 16:30:55 -0700

    Separate Babel config so we can use JSX easier(?)

---
+++ .babelrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+	plugins: [
+		[ '@wordpress/babel-plugin-import-jsx-pragma', {
+			scopeVariable: 'createElement',
+			source: '@wordpress/element',
+			isDefault: false
+		} ],
+		[ '@babel/transform-react-jsx', {
+			pragma: 'createElement'
+		} ]
+	]
+};
